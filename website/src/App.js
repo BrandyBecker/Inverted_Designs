3a5a92cba5073e0b0f8b449debb4574663afe633
BrandyBecker <brandybeckerfsw@gmail.com>
2020-12-15 17:11:04 -0500

    rough draft of home page, header, nav, footer, bio

--- website/src/App.js
+++ website/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Nav from './components/nav/nav'
 import Home from './components/home/home'
 import { Route } from "react-router-dom"; 
+import BottomNav from './components/bottomnav/bottomnav';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Route exact path='/' component={Home} />
+      <BottomNav/>
     </div>
   );
 }
---
+++ website/src/components/bottomnav/bottomnav.js
@@ -0,0 +1,17 @@
+import './bottomnav.css';
+import {Link} from 'react-router-dom';
+
+function BottomNav() {
+  return (
+    <div className="Nav">
+        <div class="iconholder">
+            <a class="icon" href="https://www.facebook.com/Inverted-Designs-103299074996813/" target="_blank"><i class="fab fa-facebook"></i></a>
+        </div>
+    </div>
+  );
+}
+
+export default BottomNav;
+
+
+  
